457e0e389e54b0081bf73d215383a49b61eadd31
Ashikur Rahman <ashikur35-2627@diu.edu.bd>
2021-02-28 15:22:09 -0800

    models file done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import post
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status", 'title')
+    search_fields = ['title', 'content']
+    prepopulated_field = {'sulg': ('title',)}
+
+admin.site.register(post, postAdmin)
