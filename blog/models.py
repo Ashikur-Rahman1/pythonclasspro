9d13248da9c920203b9b816018ad1aa61b92a78d
Ashikur Rahman <ashikur35-2627@diu.edu.bd>
2021-02-27 22:36:10 -0800

    model class completed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+status = (
+    (0, 'Unpublished'),
+    (1, "Published"),
+    (2, "Draft")
+)
 # Create your models here.
+
+class post(models.Model):
+
+
+
+    # title, slug, author, update_on, content, created_on, status
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE(), related_name="Blog post")
+    updated_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=status, default=0)
+
+
+    class Meta:
+        ordering = ['-Created_on']
+
+    def __str__(self):
+        return self.title
+
+
+
+
