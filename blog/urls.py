bda2ce64caf13eb331e84c02e380c1d18ac26839
Ashikur Rahman <ashikur35-2627@diu.edu.bd>
2021-02-28 17:17:13 -0800

    url,templates completed

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from. import views
 
 urlpattern=[
-
+    path('',views.postlist.as_view(),name='home'),
+    path('<slug:slug>/', views.postdetail.as_view(), name='postdetail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import post
 
 # Create your views here.
+
+class postlist(generic.ListView):
+    queryset = post.objects.filter(status=1).ordering_by('-created_on')
+    template_name = 'index.html'
+    class postdetails(generic.DetailView):
+        model = post
+        template_name = 'postdetails.html'
